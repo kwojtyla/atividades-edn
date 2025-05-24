857816331bd440833f039931f81527087f24440a
Karol Wojtyla <karolwojtyla360@gmail.com>
2025-05-24 16:39:40 -0300

    feat: add #6 resolution

---
+++ 06-operadores-matematicos.py
@@ -0,0 +1,15 @@
+a = 10
+b = 3
+
+print(a + b, a - b, a * b, a / b, a ** b, a // b, a % b)
+
+x = 4
+y = 8
+z = 15
+w = 3
+
+soma = x + y + z + w
+media = soma / 4
+
+print("Soma:", soma)
+print(f"Média: {media:.2f}")

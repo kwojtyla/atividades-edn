03e12d80010ffba5e8b1a2e92a03eeba7d1ba840
Karol Wojtyla <karolwojtyla360@gmail.com>
2025-05-24 16:50:18 -0300

    feat: add #11 resolution

---
+++ 11-total-compra.py
@@ -0,0 +1,10 @@
+produto = "Caderno"
+preco_unitario = 7.50
+quantidade = 4
+
+total = preco_unitario * quantidade
+
+print("Produto:", produto)
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print("Quantidade:", quantidade)
+print(f"Valor total da compra: R$ {total:.2f}")
